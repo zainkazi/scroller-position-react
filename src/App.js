838d405b6de4b8f8c4a549bcedf818af0dec49ca
Zain Kazi <zainkazi27@gmail.com>
2023-04-03 15:41:59 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const MousePosition = ({ render }) => {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    const handleMousePositionChange = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", handleMousePositionChange);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMousePositionChange);
+    };
+  }, []);
+
+  // What should be returned here?
+  return render(mousePosition);
+};
+
+const PanelMouseLogger = () => {
+  return (
+    <MousePosition
+      render={(mousePosition) => {
+        return (
+          <div className="BasicTracker">
+            <p>Mouse position:</p>
+            <div className="Row">
+              <span>x: {mousePosition.x}</span>
+              <span>y: {mousePosition.y}</span>
+            </div>
+          </div>
+        );
+      }}
+    />
+  );
+};
+
+// This component should not receive any props
+const PointMouseLogger = ({ mousePosition }) => {
+  return (
+    <MousePosition
+      render={(mousePosition) => {
+        return (
+          <p>
+            ({mousePosition.x}, {mousePosition.y})
+          </p>
+        );
+      }}
+    />
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Header">Little Lemon Restaurant 🍕</header>
+      <PanelMouseLogger />
+      <PointMouseLogger />
+    </div>
+  );
+}
+
+export default App;
